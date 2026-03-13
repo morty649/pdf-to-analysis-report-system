43751d7c68bdf2f1c4bf224acc45823b262d29dd
morty649 <enugulamaruthi@gmail.com>
2026-03-14 01:32:30 +0530

    langchain-text-splitters

--- utils/pdf_loader.py
+++ utils/pdf_loader.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from utils.preprocessing import clean_text
 from config.config import CHUNK_SIZE, CHUNK_OVERLAP
 
